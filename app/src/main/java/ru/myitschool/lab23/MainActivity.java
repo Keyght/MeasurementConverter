589d68cfbac8174031a67f4a6d2cee0088ae8cd8
Nikita <nikita_novakov@mail.ru>
2023-02-24 03:40:01 +0300

    Init project and strings.xml

---
+++ app/src/main/java/ru/myitschool/lab23/MainActivity.java
@@ -0,0 +1,29 @@
+package ru.myitschool.lab23;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.myitschool.lab23.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private int lower = 0;
+    private int upper = 1;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (getIntent().getExtras() != null) {
+            lower = getIntent().getExtras().getInt("lower", 0);
+            upper = getIntent().getExtras().getInt("upper", 1);
+        }
+
+    }
+}
