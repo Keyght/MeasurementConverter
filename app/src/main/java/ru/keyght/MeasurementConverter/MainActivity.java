2306395ba04ff75fed6ed23a994fded32ae264b6
Nikita <nikita_novakov@mail.ru>
2023-02-25 16:32:57 +0300

    Measurments done

--- app/src/main/java/ru/keyght/MeasurementConverter/MainActivity.java
+++ app/src/main/java/ru/keyght/MeasurementConverter/MainActivity.java
@@ -1,18 +1,24 @@
 package ru.keyght.MeasurementConverter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.keyght.MeasurementConverter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     private ActivityMainBinding binding;
 
-    private int lower = 0;
-    private int upper = 1;
+    private int lower = 2;
+    private int upper = 15;
+    private ViewGroup measurements;
+    private MetricsData metricsData;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,9 +27,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         if (getIntent().getExtras() != null) {
-            lower = getIntent().getExtras().getInt("lower", 0);
-            upper = getIntent().getExtras().getInt("upper", 1);
+            lower = getIntent().getExtras().getInt("lower", 2);
+            upper = getIntent().getExtras().getInt("upper", 15);
         }
 
+        metricsData = new MetricsData();
+        measurements = binding.Measurments;
+        RemoveExtraMeasures();
+
+    }
+
+    private void RemoveExtraMeasures() {
+        int metricSize = metricsData.getText_view_captions().size();
+        List<View> toRemove = new ArrayList<View>();
+        for (int i = 0; i < metricSize; i++) {
+            if (i < lower || i > upper)  {
+                toRemove.add(measurements.getChildAt(i));
+            }
+        }
+        for (View view : toRemove) {
+            measurements.removeView(view);
+        }
+    }
+
+    private void SetMeasuresSize() {
+        for (int i = 0; i < measurements.getChildCount(); i++) {
+            View currentView = measurements.getChildAt(i);
+            currentView.la
+        }
     }
 }
--- app/src/main/java/ru/keyght/MeasurementConverter/MetricsData.java
+++ app/src/main/java/ru/keyght/MeasurementConverter/MetricsData.java
@@ -1,4 +1,42 @@
 package ru.keyght.MeasurementConverter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MetricsData {
+    private Map<Integer, String> text_view_captions;
+
+    public  MetricsData() {
+        text_view_captions = new HashMap<Integer, String>();
+
+        text_view_captions.put(0, "et_inch");
+        text_view_captions.put(1, "et_yard");
+        text_view_captions.put(2, "et_foot");
+        text_view_captions.put(3, "et_mile");
+        text_view_captions.put(4, "et_yottametre");
+        text_view_captions.put(5, "et_zettametre");
+        text_view_captions.put(6, "et_exametre");
+        text_view_captions.put(7, "et_petametre");
+        text_view_captions.put(8, "et_terametre");
+        text_view_captions.put(9, "et_gigametre");
+        text_view_captions.put(10, "et_megametre");
+        text_view_captions.put(11, "et_kilometre");
+        text_view_captions.put(12, "et_hectometre");
+        text_view_captions.put(13, "et_decametre");
+        text_view_captions.put(14, "et_metre");
+        text_view_captions.put(15, "et_decimetre");
+        text_view_captions.put(16, "et_centimetre");
+        text_view_captions.put(17, "et_millimetre");
+        text_view_captions.put(18, "et_micrometre");
+        text_view_captions.put(19, "et_nanometre");
+        text_view_captions.put(20, "et_picometre");
+        text_view_captions.put(21, "et_femtometre");
+        text_view_captions.put(22, "et_attometre");
+        text_view_captions.put(23, "et_zeptometre");
+        text_view_captions.put(24, "et_yoctometre");
+    }
+
+    public Map<Integer, String> getText_view_captions() {
+        return text_view_captions;
+    }
 }
